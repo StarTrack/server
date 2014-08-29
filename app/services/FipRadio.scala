623df75d86f991696c87181f83a10ecd725cc314
Valerian Barbot <vbr@zenexity.com>
2014-08-29 11:55:32 +0200

    fip player

--- app/services/FipRadio.scala
+++ app/services/FipRadio.scala
@@ -28,14 +28,23 @@ object Track {
   implicit val reader = Json.reads[Track]
 }
 
-object FipRadio {
-  private var cachedResponse: Option[Track] = None
+case class FipPlayer(
+                      current: Track,
+                      previous1: Track,
+                      previous2: Track,
+                      next1: Track,
+                      next2: Track)
 
-  def currentTrack(): Future[Track] = if (cachedResponse.isDefined) {
+object FipRadio {
+  private var cachedResponse: Option[FipPlayer] = None
+
+  def currentTrack(): Future[Track] = getFipPlayer().map(_.current)
+
+  def getFipPlayer(): Future[FipPlayer] = if (cachedResponse.isDefined) {
     val timestamp = DateTime.now().getMillis
-    val track = cachedResponse.get
-    if (track.endTime > timestamp) {
-      Future.successful(track)
+    val currentTrack = cachedResponse.get.current
+    if (currentTrack.endTime > timestamp) {
+      Future.successful(cachedResponse.get)
     } else {
       getPlayerCurrent
     }
@@ -43,14 +52,22 @@ object FipRadio {
     getPlayerCurrent
   }
 
+
   val apiURL = "http://www.fipradio.fr/sites/default/files/import_si/si_titre_antenne/FIP_player_current.json"
 
-  private def getPlayerCurrent: Future[Track] = {
+  private def getPlayerCurrent: Future[FipPlayer] = {
     WS.url(apiURL).get().map { response =>
-      val t = response.json.\("current").\("song").as[Track]
-      println(t)
-      cachedResponse = Some(t)
-      t
+      val currentTrack = response.json.\("current").\("song").as[Track]
+      val p1Track = response.json.\("previous1").\("song").as[Track]
+      val p2Track = response.json.\("previous2").\("song").as[Track]
+      val n1Track = response.json.\("next1").\("song").as[Track]
+      val n2Track = response.json.\("next2").\("song").as[Track]
+
+      val p = FipPlayer(currentTrack, p1Track, p2Track, n1Track, n2Track)
+
+      println(p)
+      cachedResponse = Some(p)
+      p
     }
   }
 
