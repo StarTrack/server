bae24dea94074e87a93e291726444e659a169db9
Gwenaelle Georget <gge@zenexity.com>
2014-08-29 15:41:52 +0200

    home style gros

--- public/javascripts/components/Home.js
+++ public/javascripts/components/Home.js
@@ -1,8 +1,17 @@
 /** @jsx React.DOM */
 var Home = React.createClass({
   render:function(){
-    return <div className="yoers">
-      <a className="fat-button" href="/auth">LOG ME WITH SPOTIFY</a>
+    return 	<div>
+    <section className="yoers yoer10">
+    	<div className="container">
+    		<p>Fip Radio live playing tracks right in your Spotify playlist, in one tap!</p>
+    	</div>
+    </section>
+    <section className="yoers">
+      	<div className="container">
+      		<a className="fat-button" href="/auth">LOG ME WITH SPOTIFY</a>
+      	</div>
+    </section>
     </div>
-  } 
+  }
 });
