18bd3820f2ba9040755289baa230c10201a90815
Alex S <bobylito@gmail.com>
2014-08-29 17:06:36 +0200

    init value yo account

--- public/javascripts/components/Cockpit.js
+++ public/javascripts/components/Cockpit.js
@@ -14,7 +14,7 @@ var Cockpit = React.createClass({
       <section className="section green">
         <div class="container">
           <p>IF
-            <input autofocus className="yoUser" type="text" placeholder="Yo username" onKeyUp={debouncedSend}/>
+            <input autofocus className="yoUser" type="text" placeholder="Yo username" onKeyUp={debouncedSend} value={this.props.user.yoAccounts || ""}/>
             Yoes FIP <br/>
             then save the current track <br/>to this playlist&nbsp;
             <select className="playlist" onChange={this.sendYoers}>
