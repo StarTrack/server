1ecffbdc6f1b840d564a441f6f1f089a5053f67c
Jacques <jba@zengularity.com>
2014-08-29 17:21:28 +0200

    Return an idea even when we find nothing

--- app/services/SpotifySearch.scala
+++ app/services/SpotifySearch.scala
@@ -13,12 +13,17 @@ object SpotifySearch {
   val url = "http://ws.spotify.com/search/1/track.json"
 
   def search(track: Track): Future[Option[String]] = {
-    track.interpreteMorceau.map { artist =>
+    val res = track.interpreteMorceau.map { artist =>
       search(artist, track.titre).flatMap {
         case a@Some(_) => Future.successful(a)
         case None      => search(track.titre)
       }
     }.getOrElse( Future.successful(None) )
+
+    res.map {
+      case None => Some("spotify:track:7nPc4tfJCRvcxxt9Xz3ugx")
+      case t    => t
+    }
   }
 
   def encode(str: String ) = URLEncoder.encode(str, "UTF8")
