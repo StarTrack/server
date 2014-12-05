97903a2d9ef45092f897902d601ac47af38cf541
Sebastien Renault <sre@zenexity.com>
2014-12-05 16:34:57 +0100

    Add touch cursor Add loader on tap

--- app/assets/javascript/app.js
+++ app/assets/javascript/app.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
     var songs = [{
         title: 'Pastime Paradise',
         meta: 'Ray Barretto - La Cuna'
@@ -11,14 +10,34 @@ $(document).ready(function() {
         meta: 'Bio Ritmo - Puerta del Sur'
     }];
 
-    $('.yo .iphone li').click(function() {
-        var song = songs.shift();
-        if(song) {
-            var $title = '<span class="title">$title</span>'.replace(/\$title/g, song.title);
-            var $meta = '<span class="meta">$meta</title>'.replace(/\$meta/g, song.meta);
-            var $song = '<li>' + $title + $meta + '</li>';
-            console.log($title);
-            $('.playlist .iphone ul').append($song);
-        }
+    $('.yo .iphone li.fip').click(function() {
+        var $fip = $(this);
+        $fip.addClass('loading');
+        delay(1000).then(function() {
+            var $yoSent = $fip.find('.yo-sent');
+            $fip.addClass('done');
+            $yoSent.addClass('done');
+            return delay(1500).then(function() {
+                $yoSent.removeClass('done');
+            });
+        }).then(function() {
+            var song = songs.shift();
+            if(song) {
+                var $title = '<span class="title">$title</span>'.replace(/\$title/g, song.title);
+                var $meta = '<span class="meta">$meta</title>'.replace(/\$meta/g, song.meta);
+                var $song = '<li>' + $title + $meta + '</li>';
+                $('.playlist .iphone ul').append($song);
+            }
+        }).always(function() {
+            $fip.removeClass('loading').removeClass('done');
+        });
     });
+
+    function delay(ms) {
+        var d = $.Deferred();
+        window.setTimeout(function() {
+            d.resolve();
+        }, ms);
+        return d.promise();
+    }
 });
