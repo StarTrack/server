ea8ab6d98f46d13972277234804dd18873075bf6
Sebastien Renault <sre@zenexity.com>
2014-12-05 22:47:41 +0100

    WIP animation

--- app/assets/javascript/app.js
+++ app/assets/javascript/app.js
@@ -1,37 +1,84 @@
 $(document).ready(function() {
-    var songs = [{
-        title: 'Pastime Paradise',
-        meta: 'Ray Barretto - La Cuna'
-    },{
-        title: 'Wicked Game',
-        meta: 'Chris Isaak - No Begining No End'
-    },{
-        title: 'Le Dicen Dolor',
-        meta: 'Bio Ritmo - Puerta del Sur'
-    }];
+    //// DOM
+
+    var $fip = function() {
+        return $('.fip');
+    };
+
+    var $fipPoint = function() {
+        return $('.fip').find('.point');
+    };
+
+    var $yoSent = function() {
+        return $fip().find('.yo-sent');
+    };
+
+    var $yoMask = function() {
+        return $('.yo .mask');
+    };
+
+    var $playlist = function() {
+        return $('.playlist');
+    };
+
+    var $playlistMask = function() {
+        return $playlist().find('.mask');
+    };
+
+    //// DEMO
+    function sendYo() {
+        return delay(1000).then(function() {
+            $fip().addClass('done');
+            $yoSent().addClass('done');
+        }).then(function() {
+            return delay(1500).then(function() {
+                $yoSent().removeClass('done');
+            });
+        });
+    }
+
+    function fadeInPlaylist() {
+        return delay(1000).then(function() {
+            $playlistMask().removeClass('on');
+            $playlistMask().addClass('off');
+        });
+    }
+
+    function fadeOutYo() {
+        return delay(10).then(function() {
+            $fip().removeClass('loading').removeClass('done');
+            $fipPoint().addClass('disabled');
+            $yoMask().addClass('on');
+            $yoMask().removeClass('off');
+        });
+    }
+
+    function addToPlaylist() {
+        var song = {
+            title: 'Pastime Paradise',
+            meta: 'Ray Barretto - La Cuna'
+        };
+
+        if(song) {
+            var $title = '<span class="title">$title</span>'.replace(/\$title/g, song.title);
+            var $meta = '<span class="meta">$meta</title>'.replace(/\$meta/g, song.meta);
+            var $song = '<li>' + $title + $meta + '</li>';
+            $playlist().find('ul').append($song);
+        }
+    }
+
+    //// EVENTS
 
     $('.yo .iphone li.fip').click(function() {
-        var $fip = $(this);
-        $fip.addClass('loading');
-        delay(1000).then(function() {
-            var $yoSent = $fip.find('.yo-sent');
-            $fip.addClass('done');
-            $yoSent.addClass('done');
-            return delay(1500).then(function() {
-                $yoSent.removeClass('done');
-            });
+        $fip().addClass('loading');
+        sendYo().then(function() {
+            return $.when(fadeInPlaylist(), fadeOutYo());
         }).then(function() {
-            var song = songs.shift();
-            if(song) {
-                var $title = '<span class="title">$title</span>'.replace(/\$title/g, song.title);
-                var $meta = '<span class="meta">$meta</title>'.replace(/\$meta/g, song.meta);
-                var $song = '<li>' + $title + $meta + '</li>';
-                $('.playlist .iphone ul').append($song);
-            }
-        }).always(function() {
-            $fip.removeClass('loading').removeClass('done');
+            return addToPlaylist();
         });
     });
+
+    //// UTILS
 
     function delay(ms) {
         var d = $.Deferred();
